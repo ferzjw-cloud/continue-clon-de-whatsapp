9d0b25b5cabefdda8941fcd3d519ae888a4d89ef
ferzjw <ferz.jw@gmail.com>
2026-01-15 12:33:38 -0300

    add: cambios en formulario

--- src/app/main.js
+++ src/app/main.js
@@ -1,1 +1,35 @@
-alert ("!")
+// referenciar elementos a manipular del html
+const $search = document.getElementById("search")
+const $chats = document.querySelectorAll(".user-chat")
+
+const searchChats = () => {
+    const nameSearch = $search.value.toLowerCase()
+
+    for (let i = 0; i < $chats.length; i++) {
+        const $chat = $chats [i]
+        const $titleTag = $chat.querySelector("h3")
+        const name = $titleTag.textContent.toLowerCase()
+        
+        //console.log (name, "donde va a buscar el user")
+
+        if(name.includes(nameSearch)) {
+            $chat.style.display = "flex"
+        } else {$chat.style.display = "none"
+
+
+
+    // Concepto: El método includes() en JavaScript determina si un elemento específico se encuentra dentro de un arreglo o una cadena de texto, devolviendo un valor booleano (true o false) según sea el caso.
+
+        }
+
+    }
+
+    //console.log (nameSearch, "lo que va a buscar en el user")
+}
+
+// Validar la longitud de $chats
+
+const lenghtChats = $chats.length
+console.log (lenghtChats)
+
+$search.addEventListener("input",searchChats) 
